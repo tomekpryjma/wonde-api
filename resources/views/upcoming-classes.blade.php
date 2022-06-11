c469971fecb095fe84b4ca2a1358952576ce2f6b
Tomek Pryjma <tomek.pryjma@gmail.com>
2022-06-11 15:31:49 +0100

    Refactor blade views

--- resources/views/upcoming-classes.blade.php
+++ resources/views/upcoming-classes.blade.php
@@ -1,19 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Upcoming Classes') }}</div>
-            </div>
-        </div>
-    </div>
-    
     <upcoming-lessons
         employee-id="{{ $employeeId }}"
         api-url="{{ config('app.wonde.api_url') }}"
         school-id="{{ config('app.wonde.school_id') }}"
         token="{{ config('app.wonde.auth_token') }}"/>
-</div>
 @endsection
